f9a6d6f946b5d198fccc6f672f204c49af13dd00
maortoubian <maortoubian@gmail.com>
2015-12-20 00:10:09 +0200

    index

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 /* DONOT forget (cmd, at project folder): $npm install express -- save */
 var express = require('express');
 var app = express();
-app.use('/', express.static('./public')).listen(8080);
+app.use('/', express.static('./public')).listen(process.env.PORT);
 console.log("We have connection! 8080");
